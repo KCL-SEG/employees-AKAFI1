cb484bf979e3f777988df46ef3cb71c0291e90be
Kafi <ahmed.kafi@outlook.com>
2022-12-05 13:54:42 +0000

    employee code

--- employee.py
+++ employee.py
@@ -2,14 +2,48 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name):
+    def __init__(self, name, contract="", hours="", commission="", hourlyPay="", salaryPay="", fixedCommission="", noContracts="", contractPay=""):
         self.name = name
+        self.contract = contract
+        self.hours = hours
+        self.commission = commission
+        self.hourlyPay = hourlyPay
+        self.salaryPay = salaryPay
+        self.fixedCommission = fixedCommission
+        self.noContracts = noContracts
+        self.contractPay = contractPay
+
+    def contract_pay(self):
+        if self.contract == 'monthly':
+            return self.salaryPay
+        else:
+            return self.hourlyPay * self.hours
+
+    def commission_pay(self):
+        if self.commission == 'bonus':
+            return self.fixedCommission
+        else:
+            return self.noContracts * self.contractPay
 
     def get_pay(self):
-        pass
+        totalPay = self.contract_pay()
+        if self.commission:
+            totalPay += self.commission_pay()
+        return totalPay
 
     def __str__(self):
-        return self.name
+        printStr = f"{self.name} works on a "
+        if self.contract == 'monthly':
+            printStr += f"{self.name} has a monthly salary of {self.contractPay}"
+        else:
+            printStr += f"{self.name} has a contract of {self.hours} hours at {self.hourlyPay}/hour"
+        if self.commission:
+            if self.commission == 'bonus':
+                printStr += f" and receives a bonus commission of {self.fixedCommission}."
+            else:
+                printStr += f" and receives a commission for {noContracts} contract(s) at {contractPay}/contract."
+        printStr += f" {self.name}'s total pay is {self.get_pay()}."
+        print(printStr)
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.

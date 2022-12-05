6aecb36633ead3832cbcaf1db3245d23157dd1a4
Kafi <ahmed.kafi@outlook.com>
2022-12-05 14:04:26 +0000

    employees

--- employee.py
+++ employee.py
@@ -1,18 +1,18 @@
 """Employee pay calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-class FixedCommission:
+class FixedComm:
     def __init__(self, bonus) -> None:
         self.description = f"bonus commission of {bonus}"
         self.value = bonus
 
-class ContractCommission:
+class ContractComm:
     def __init__(self, contract_num, price_per_contract) -> None:
         self.description = f"commission for {contract_num} contract(s) at {price_per_contract}/contract"
         self.value = contract_num * price_per_contract
 
 class Employee:
-    def __init__(self, name):
+    def __init__(self, name, commission):
         self.name = name
 
     def get_pay(self):
@@ -48,19 +48,19 @@ class HourlyEmployee(Employee):
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
+billie = SalaryEmployee("Billie", 4000)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie')
+charlie = HourlyEmployee("Charlie", 100, 25)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee')
+renee = SalaryEmployee("Renee", 3000, ContractComm(4, 200))
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan')
+jan = HourlyEmployee("Jan", 150, 25, ContractComm(3, 220))
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie')
+robbie = SalaryEmployee("Robbie", 2000, FixedComm(1500))
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel')
+ariel = HourlyEmployee("Ariel", 120, 30, FixedComm(600))
